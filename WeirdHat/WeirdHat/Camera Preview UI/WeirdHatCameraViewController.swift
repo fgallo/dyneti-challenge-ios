4a9d3df7583f5ad591a8340efd9b6aa47165988a
Fernando Gallo <fernandgallo@gmail.com>
2023-06-30 11:06:13 -0300

    Refactor UI

--- WeirdHat/WeirdHat/Camera Preview UI/WeirdHatCameraViewController.swift
+++ WeirdHat/WeirdHat/Camera Preview UI/WeirdHatCameraViewController.swift
@@ -5,12 +5,17 @@
 import UIKit
 
 class WeirdHatCameraViewController: UIViewController {
+    @IBOutlet private(set) var takePictureButton: UIButton!
+    @IBOutlet private(set) var flipCameraButton: UIButton!
+    @IBOutlet private(set) var infoButton: UIButton!
+    
     var cameraPreview: AVCaptureSessionCameraPreview!
     
     private var model = [UIImageView]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureButtons()
         configureCameraPreviewLayer()
         binding()
     }
@@ -42,6 +47,19 @@ class WeirdHatCameraViewController: UIViewController {
         cameraPreview.previewLayer.videoGravity = .resizeAspectFill
         view.layer.insertSublayer(cameraPreview.previewLayer, at: 0)
         cameraPreview.previewLayer.frame = view.frame
+    }
+    
+    private func configureButtons() {
+        takePictureButton.setImage(symbolFor(name: "circle.inset.filled", size: 80), for: .normal)
+        flipCameraButton.setImage(symbolFor(name: "arrow.triangle.2.circlepath.camera", size: 20), for: .normal)
+        infoButton.setImage(symbolFor(name: "info.circle", size: 20), for: .normal)
+    }
+    
+    private func symbolFor(name: String, size: CGFloat) -> UIImage? {
+        let configuration = UIImage.SymbolConfiguration(pointSize: size, weight: .regular, scale: .large)
+        let image = UIImage(systemName: name, withConfiguration: configuration)?
+            .withTintColor(.white, renderingMode: .alwaysOriginal)
+        return image
     }
     
     private func handleFaceDetectionResults(observedFacesBoundingBoxes: [CGRect]) {
